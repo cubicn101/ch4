369a75ecf71726a7553cff43d95aeeee1d881eea
cubicn101 <yuxinxia702@gmail.com>
2026-09-22 14:56:37 -0700

    Add files via upload

---
+++ FortmatDate.java
@@ -0,0 +1,15 @@
+public static void FormatDate
+
+	
+public static void printAmerican(????){	
+	System.out
+}
+
+
+public static void main(String[] args) {
+    String day = Tuseday;
+    String month = September;
+    int date = 22;
+    int year = 2026;
+    printAmerican()
+}
