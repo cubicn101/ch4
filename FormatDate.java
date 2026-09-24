36131dc316a675ac0ab938551f61ff418b52e33b
cubicn101 <yuxinxia702@gmail.com>
2026-09-24 13:46:06 -0700

    Add files via upload

--- FormatDate.java
+++ FormatDate.java
@@ -6,13 +6,14 @@ int date, int year){
 	System.out.println(day+", "+ month + date+ ", "+ year);
 }
 
-public static void printeEuropean(int Month, int date, int year){
-	System.out.println(date+"/"+ Month + "/" + year);
-}
+public static void printEuropean(String Month, int date, int year){
+	System.out.println(date+" "+ Month + ", " + year);
+	// i dont know where the day should go for European time format :(
+}   
 
 
 public static void main(String[] args) {
   printAmerican("Wednesday", "September", 23, 2026 );
-  printEuropean(09, 23, 2026);
+  printEuropean("September", 23, 2026);
 }
 }
