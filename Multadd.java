baa4b8a5a1a72a4fd565309cd1dceeae9585c2ac
cubicn101 <yuxinxia702@gmail.com>
2026-09-24 14:24:11 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,16 @@
+public class Multadd{
+	//4.6 exercise
+
+public static double multadd(double a, double b, double c){
+	
+	
+}
+public static double expSum()	
+	double x = 
+
+
+public static void main {
+	multadd();
+	
+}
+}
